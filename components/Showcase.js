c697a6425ef3328129715ca83f8ff1d39c33ae62
unknown <engel199702@hotmail.com>
2020-01-12 16:58:27 -0600

    showcase home

---
+++ components/Showcase.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Link from 'next/link';
+import { Row, Col } from 'reactstrap';
+
+function Showcase({ data }) {
+    return (
+        <Row>
+            {data.items.map(item => (
+                <Col md={3}>
+                    <div className="course-feature text-center bg-white rounded p-4 pt-5 pb-5"
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column'
+                        }}>
+                        <div style={{display:'flex',justifyContent:'center'}} >
+                            <div style={{
+                                background: '#2f55d4',
+                                width: 65,
+                                height: 65,
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                borderRadius: '50%'
+                            }}>
+                                <img src={item.icon && item.icon.responsiveImage.src}
+                                    srcSet={item.icon && item.icon.responsiveImage.srcSet} height="35" alt="" />
+                            </div>
+                        </div>
+                        <h4 className="mt-3"><Link href={item.url}><a className="title text-dark">{item.title}</a></Link></h4>
+                        <Link href={item.url} ><a className="text-primary read-more">Read More <i className="fas fa-angle-right"></i></a></Link>
+                        <img src={item.icon && item.icon.responsiveImage.src}
+                            srcSet={item.icon && item.icon.responsiveImage.srcSet} className="full-img" height="300" alt="" />
+                    </div>
+                </Col>
+            ))}
+        </Row>
+    )
+}
+
+export default Showcase;
--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,7 @@ import { Row, Col } from 'reactstrap';
 import BlogCarousel from '../components/Carousel/BlogCarousel';
 import InfiniteCarousel from '../components/Carousel/InfiniteCarousel';
 import SliderHome from '../components/Carousel/SliderHome';
-
-// Import images
-import homeShape from '../template/images/saas/home-shape.png';
-
-// import images
-import insurance from "../template/images/icon/insurance.svg";
-import graduationHat from "../template/images/icon/graduation-hat.svg";
-import ai from "../template/images/icon/ai.svg";
+import Showcase from '../components/Showcase';
 
 const GET_PAGE = gql`
   query getPage($slug: String!) {
@@ -31,6 +24,22 @@ const GET_PAGE = gql`
         }
       }
       content {
+        ... on ShowcaseReferenceRecord {
+          showcase {
+            id
+            items {
+              id
+              title
+              icon {
+                responsiveImage {
+                  src
+                  srcSet
+                }
+              }
+              url
+            }
+          }
+        }
         ... on CarouselReferenceRecord {
           carousel {
             id
@@ -67,10 +76,10 @@ const Home = () => {
     }
   });
 
-  useEffect(() => {
-    // console.log(router.query.slug)
-    console.log(data)
-  }, [data])
+  // useEffect(() => {
+  //   // console.log(router.query.slug)
+  //   console.log(data)
+  // }, [data])
 
   useEffect(() => {
     document.body.classList = "";
@@ -96,7 +105,9 @@ const Home = () => {
 
         {content.map(contentModule => {
           switch (contentModule.__typename) {
+
             case "CarouselReferenceRecord":
+
               switch (contentModule.carousel.template.name) {
                 case 'Blog':
                   return (
@@ -111,57 +122,23 @@ const Home = () => {
                     </section>
                   )
                 case 'Home':
-                  return (
-                    <React.Fragment>
-                    <SliderHome data={contentModule.carousel} />
-                    <section className="section">
-                      <div className="container">
-                        <Row>
-                          <Col md={4}>
-                            <div className="course-feature text-center position-relative d-block overflow-hidden bg-white rounded p-4 pt-5 pb-5">
-                              <div className="icon">
-                                <img src={insurance} height="55" alt="" />
-                              </div>
-                              <h4 className="mt-3"><Link href="#" className="title text-dark"> Unlimited Access</Link></h4>
-                              <p className="text-muted">It is a long established fact that a reader will be of a page reader will be of a page when looking at its layout. </p>
-                              {/* <Link href="#" className="text-primary read-more">Read More <i className="mdi mdi-chevron-right"></i></Link> */}
-                              <img src={insurance} className="full-img" height="300" alt="" />
-                            </div>
-                          </Col>
-
-                          <Col md={4}>
-                            <div className="course-feature text-center position-relative d-block overflow-hidden bg-white rounded p-4 pt-5 pb-5">
-                              <div className="icon">
-                                <img src={graduationHat} height="55" alt="" />
-                              </div>
-                              <h4 className="mt-3"><Link href="#" className="title text-dark"> Our Courses</Link></h4>
-                              <p className="text-muted">It is a long established fact that a reader will be of a page when reader will be of a page looking at its layout. </p>
-                              {/* <Link href="#" className="text-primary read-more">Read More <i className="mdi mdi-chevron-right"></i></Link> */}
-                              <img src={graduationHat} className="full-img" height="300" alt="" />
-                            </div>
-                          </Col>
-
-                          <Col md={4}>
-                            <div className="course-feature text-center position-relative d-block overflow-hidden bg-white rounded mb-0 p-4 pt-5 pb-5">
-                              <div className="icon">
-                                <img src={ai} height="55" alt="" />
-                              </div>
-                              <h4 className="mt-3"><Link href="#" className="title text-dark"> Expert Teachers</Link></h4>
-                              <p className="text-muted">It is a long established fact that a reader will be of a page when reader will be of a page looking at its layout. </p>
-                              {/* <Link href="#" className="text-primary read-more">Read More <i className="mdi mdi-chevron-right"></i></Link> */}
-                              <img src={ai} className="full-img" height="300" alt="" />
-                            </div>
-                          </Col>
-                        </Row>
-                      </div>
-                    </section>
-                    </React.Fragment>
-                  )
+                  return <SliderHome data={contentModule.carousel} />
                 default:
                   return null;
               }
+
+            case 'ShowcaseReferenceRecord':
+              return (
+                <section className="section">
+                  <div className="container">
+                    <Showcase data={contentModule.showcase} />
+                  </div>
+                </section>
+              )
+
             default:
               return null;
+
           }
         })}
 
