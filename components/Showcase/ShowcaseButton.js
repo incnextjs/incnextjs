b76d4d2136e527332daf21b1740dd77f42224146
unknown <engel199702@hotmail.com>
2020-01-15 21:43:11 -0600

    showcase button link whole component

--- components/Showcase/ShowcaseButton.js
+++ components/Showcase/ShowcaseButton.js
@@ -7,15 +7,17 @@ function ShowcaseButton({ data }) {
         <Row>
             {data.items.map(item => (
                 <Col key={item.id} lg={6} md={6} xs={12} className="mt-4 pt-2">
-                    <div className="key-feature d-flex p-3 rounded bg-white shadow">
-                        <div className="icon text-center rounded-pill mr-3">
-                            {/* <i className="fas fa-user-friends text-primary"></i> */}
-                            <img src={item.icon && item.icon.url} style={{ height: '55%' }} />
+                    <a href={item.url}>
+                        <div className="key-feature d-flex p-3 rounded bg-white shadow">
+                            <div className="icon text-center rounded-pill mr-3">
+                                {/* <i className="fas fa-user-friends text-primary"></i> */}
+                                <img src={item.icon && item.icon.url} style={{ height: '55%' }} />
+                            </div>
+                            <div className="content mt-2">
+                                <h5 className="title mb-0">{item.title}</h5>
+                            </div>
                         </div>
-                        <div className="content mt-2">
-                            <h5><a href={item.url} className="title mb-0">{item.title}</a></h5>
-                        </div>
-                    </div>
+                    </a>
                 </Col>
             ))}
         </Row>
